df9149c72a3c60af3ec1d3b6ad489eeaeeb124aa
musabayramli <musabayramli2020@gmail.com>
2025-11-19 01:46:36 +0400

    update project

--- src/context/BookingContext.tsx
+++ src/context/BookingContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useReducer, ReactNode } from "react";
+import { createContext, useContext, useReducer, type ReactNode } from "react";
 import type {
   BookingState,
   DailySelection,
